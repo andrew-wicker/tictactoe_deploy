b9413a005b1722c1e2cbe650fa554063296e1ea7
Andrew <apwicker@gmail.com>
2023-08-26 16:02:15 -0700

    Fixing Links

--- src/components/row.jsx
+++ src/components/row.jsx
@@ -1,17 +1,11 @@
 import React, { useEffect, useState } from 'react';
-// eslint-disable-next-line import/no-unresolved
+
 import Box from './box.jsx';
 
 export default function Row(props) {
-  const boxMaker = (boxState, index) => (
-    <Box
-      key={index}
-      boxState={boxState}
-      onClick={() => props.onUpdateBoxState(index)}
-    />
-  );
+	const boxMaker = (boxState, index) => <Box key={index} boxState={boxState} onClick={() => props.onUpdateBoxState(index)} />;
 
-  return <div className="boxRowDiv">{props.rowStates.map(boxMaker)}</div>;
+	return <div className='boxRowDiv'>{props.rowStates.map(boxMaker)}</div>;
 }
 
 // boxStates = [" ", ' ', ' '];
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
-// import Row from './components/row.jsx';
-import Board from './components/board.jsx';
 
+import Board from 'src/components/board.jsx';
 const App = () => {
-  return (
-    <div className="rootDiv">
-      <div id="ticTacToeDiv">
-        <h1>Tic Tac Toe</h1>
-      </div>
+	return (
+		<div className='rootDiv'>
+			<div id='ticTacToeDiv'>
+				<h1>Tic Tac Toe</h1>
+			</div>
 
-      {/* <div className="ticTacToe"> */}
-      <Board />
-      {/* <Row />
-        <Row />
-        <Row /> */}
-      {/* </div> */}
-    </div>
-  );
+			<Board />
+		</div>
+	);
 };
 
 const root = createRoot(document.querySelector('#root'));
